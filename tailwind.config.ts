4f5f66d9cdf6d0c29628d522d5266be62930da28
Muhammad Abdulmalik <hackobansa@gmail.com>
2025-08-31 14:22:02 +0000

    allow all necessary packages to relevant part of the website display bef

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
+        body: ['"PT Sans"', 'sans-serif'],
+        headline: ['"PT Sans"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
